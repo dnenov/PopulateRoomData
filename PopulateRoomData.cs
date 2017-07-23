ac216955f766914b2caa66f74abfa24178b4fcb9
2adicted <dido.nenov@gmail.com>
2017-07-23 11:09:26 +0100

    Populate room data as Zero Touch Dynamo node

---
+++ PopulateRoomData.cs
@@ -0,0 +1,101 @@
+﻿using Autodesk.DesignScript.Runtime;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using RevitServices.Persistence;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Proto = Revit.Elements;
+
+namespace DynamoAecom
+{
+    [Transaction(TransactionMode.Manual)]
+    public class PopulateRoomData
+    {
+        internal PopulateRoomData()
+        {
+
+        }
+        public static string Populate([DefaultArgument("{}")] IList rooms, 
+            [DefaultArgument("{}")] IList<IList> points, 
+            [DefaultArgument("{}")] IList elements,
+            [DefaultArgument("{}")] string parameter)
+        {
+            int iterations = rooms.Count * elements.Count;
+            int iteration = 0;
+
+            using (AdnRme.ProgressForm form = new AdnRme.ProgressForm("Clear parameter value.", "Processing {0} out of " + elements.Count.ToString() + " elements", elements.Count))
+            {
+                RevitServices.Transactions.TransactionManager.Instance.EnsureInTransaction(DocumentManager.Instance.CurrentUIDocument.Document);
+
+                foreach (var el in elements)
+                {
+                    Element element = ((Proto.Element)el).InternalElement;
+                    element.LookupParameter(parameter).Set("");
+                }
+
+                form.Increment();
+                iteration++;
+
+                RevitServices.Transactions.TransactionManager.Instance.TransactionTaskDone();
+            }
+
+            
+            using (AdnRme.ProgressForm form = new AdnRme.ProgressForm("Populating Room Data", "Processing {0} out of " + iterations.ToString() + " elements", iterations))
+            {
+                iteration = 0;
+                RevitServices.Transactions.TransactionManager.Instance.EnsureInTransaction(DocumentManager.Instance.CurrentUIDocument.Document);
+
+                for (int i = 0; i < elements.Count; i++)
+                {
+                    Element el = ((Proto.Element)elements[i]).InternalElement;
+                    Parameter param = el.LookupParameter(parameter);
+                    if (param == null)
+                    {
+                        continue;
+                    }
+                    foreach (var room in rooms)
+                    {                    
+                        if(form.getAbortFlag())
+                        {
+                            return "Aborted by user";
+                        }
+
+                        Autodesk.Revit.DB.Architecture.Room r = ((Proto.Element)room).InternalElement as Autodesk.Revit.DB.Architecture.Room;
+
+                        AssignRoom(r, el, points[i], param);
+
+                        form.Increment();
+                        iteration++;
+                    }
+                    if (param.AsString().Equals(""))
+                    {
+                        param.Set("n/a");
+                    }
+                }                    
+
+                RevitServices.Transactions.TransactionManager.Instance.TransactionTaskDone();
+            }
+            
+            return String.Format("{0} elements processed successfully.", iterations.ToString());
+        }
+        
+        private static void AssignRoom(Autodesk.Revit.DB.Architecture.Room room, Element element, IList points, Parameter parameter)
+        {            
+            foreach(var point in points)
+            {
+                if (room.IsPointInRoom(point as XYZ))
+                {
+                    string name = room.LookupParameter("Number").AsString() + " - " + room.LookupParameter("Name").AsString();
+                    if (!parameter.AsString().Equals(""))
+                    {
+                        name += ", " + parameter.AsString();
+                    }
+                    parameter.Set(name);
+                    return;
+                }
+            }            
+        }
+    }
+}
---
+++ ProgressForm.designer.cs
@@ -0,0 +1,91 @@
+namespace AdnRme
+{
+    partial class ProgressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAbort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(11, 25);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(406, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Processing...";
+            // 
+            // btnAbort
+            // 
+            this.btnAbort.Location = new System.Drawing.Point(177, 57);
+            this.btnAbort.Name = "btnAbort";
+            this.btnAbort.Size = new System.Drawing.Size(75, 23);
+            this.btnAbort.TabIndex = 2;
+            this.btnAbort.Text = "Cancel";
+            this.btnAbort.UseVisualStyleBackColor = true;
+            this.btnAbort.Click += new System.EventHandler(this.btnAbort_Click_1);
+            // 
+            // ProgressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 91);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnAbort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ProgressForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Working...";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnAbort;
+    }
+}
